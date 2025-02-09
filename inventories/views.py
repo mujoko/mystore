3f7148b5c241ea14dff05ac618213dbe060e5e7b
Mujoko <mujoko.mujoko@gmail.com>
2025-02-09 15:44:40 +0700

    Initial

---
+++ inventories/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def landingpage(request):
+    return HttpResponse("<h2 style='color:blue;'>Hello, Ini Halaman Saya.</h2> <p style='color:red;'> line ke dua <p> <img src='https://www.w3schools.com/html/pic_trulli.jpg' > " )
